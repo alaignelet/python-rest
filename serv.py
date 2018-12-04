2da7dafdeb12e253a68c47168e0cf1f3432b9bdb
Alexis Laignelet <alexis.laignelet@gmail.com>
2018-12-04 01:34:43 +0000

    statistic actually working

--- serv.py
+++ serv.py
@@ -3,81 +3,7 @@ import os
 import json
 import re
 import numpy as np
-
-def nbrecur(pathaccess, number):
-    if len(os.listdir(pathaccess)) == 0:
-        return number
-    else:
-        if len(os.listdir(pathaccess)) == len([name for name in os.listdir(pathaccess) if os.path.isfile(pathaccess + '/' + name)]):
-            number += len(os.listdir(pathaccess))
-            return number
-        else:
-            for name in os.listdir(pathaccess):
-                if os.path.isfile(pathaccess + '/' + name):
-                    number += 1
-                else:
-                    return nbrecur(pathaccess + '/' + name, number)
-
-def bytesrecur(pathaccess, number):
-    if len(os.listdir(pathaccess)) == 0:
-        return number
-    else:
-        if len(os.listdir(pathaccess)) == len([name for name in os.listdir(pathaccess) if os.path.isfile(pathaccess + '/' + name)]):
-            for name in os.listdir(pathaccess):
-                number += os.path.getsize(pathaccess + '/' + name)
-            return number
-        else:
-            for name in os.listdir(pathaccess):
-                if os.path.isfile(pathaccess + '/' + name):
-                    number += os.path.getsize(pathaccess + '/' + name)
-                else:
-                    return bytesrecur(pathaccess + '/' + name, number)
-     
-def listrecur(pathaccess, liststock):
-    if len(os.listdir(pathaccess)) == 0:
-        return liststock
-    else:
-        if len(os.listdir(pathaccess)) == len([name for name in os.listdir(pathaccess) if os.path.isfile(pathaccess + '/' + name)]):
-            for name in os.listdir(pathaccess):
-                with open(pathaccess + '/' + name, 'r') as f:
-                    file_content = f.read()
-                    file_alpha_num = re.sub(r'[^a-zA-Z]', "", file_content)
-                    liststock.append(len(file_alpha_num))
-            return liststock
-        else:
-            for name in os.listdir(pathaccess):
-                if os.path.isfile(pathaccess + '/' + name):
-                    with open(pathaccess + '/' + name, 'r') as f:
-                        file_content = f.read()
-                        file_alpha_num = re.sub(r'[^a-zA-Z]', "", file_content)
-                        liststock.append(len(file_alpha_num))
-                else:
-                    return listrecur(pathaccess + '/' + name, liststock)
-
-
-def listwordrecur(pathaccess, liststock):
-    if len(os.listdir(pathaccess)) == 0:
-        return liststock
-    else:
-        if len(os.listdir(pathaccess)) == len([name for name in os.listdir(pathaccess) if os.path.isfile(pathaccess + '/' + name)]):
-            for name in os.listdir(pathaccess):
-                with open(pathaccess + '/' + name, 'r') as f:
-                    file_content = f.read()
-                    file_alpha_num = re.sub(r'([^\s\w]|_)+', '', file_content)
-                    for x in file_alpha_num.split():
-                        liststock.append(len(x))
-            return liststock
-        else:
-            for name in os.listdir(pathaccess):
-                if os.path.isfile(pathaccess + '/' + name):
-                    with open(pathaccess + '/' + name, 'r') as f:
-                        file_content = f.read()
-                        file_alpha_num = re.sub(r'([^\s\w]|_)+', '', file_content)
-                        for x in file_alpha_num.split():
-                            liststock.append(len(x))
-                else:
-                    return listwordrecur(pathaccess + '/' + name, liststock)
-
+import glob
 
 app = Flask(__name__)
 
@@ -126,25 +52,41 @@ def deletefile():
 def statistics():
     path = request.args.get('path')
     stat = request.args.get('stat')
+
+    list_total = glob.glob(path+'/**/*', recursive=True)
+    list_file = [name for name in list_total if os.path.isfile(name)]
     
     if stat == '1': #Total number of files in that folder
-        nb_file = nbrecur(path, 0)
+        nb_file = len(list_file)
         return 'NUMBER OF FILES: ' + str(nb_file)
 
     if stat == '2': #Average number of alphanumeric characters per text file (and standard deviation) in that folder
-        list_len = listrecur(path, [])
+        list_len =[]
+        for file in list_file:
+            with open(file, 'r') as f:
+                file_content = f.read()
+                file_alpha_num = re.sub(r'[^a-zA-Z]', "", file_content)
+                list_len.append(len(file_alpha_num))
         mean = np.mean(list_len)
         standard_dev = np.std(list_len)
         return 'NUMBER OF ALPHANUMERIC CHARACTERS PER TEXT FILE: \n' + 'MEAN: '+ str(round(mean,2)) + '\n' + 'STANDARD DEVIATION: ' + str(round(standard_dev,2))
 
     if stat == '3': #Average word length (and standard deviation) in that folder
-        list_len = listwordrecur(path, [])
+        list_len =[]
+        for file in list_file:
+            with open(file, 'r') as f:
+                file_content = f.read()
+                file_alpha_num = re.sub(r'([^\s\w]|_)+', '', file_content)
+                for x in file_alpha_num.split():
+                    list_len.append(len(x))
         mean = np.mean(list_len)
         standard_dev = np.std(list_len)
         return 'AVERAGE WORD LENGTH: \n' + 'MEAN: '+ str(round(mean,2)) + '\n' + 'STANDARD DEVIATION: ' + str(round(standard_dev,2))
 
     if stat == '4': #Total number of bytes stored in that folder
-        size = bytesrecur(path, 0)
+        size = 0
+        for file in list_file:
+            size += os.path.getsize(file)
         return 'TOTAL NUMBER OF BYTES: ' + str(size)
 
 if __name__ == '__main__':
